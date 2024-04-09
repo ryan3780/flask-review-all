d0cd9b054732216c7690400443a38746e30020e8
ryan3780 <guemhong3780@gmail.com>
2024-04-09 20:56:29 +0900

    Add bs4

--- api/index.py
+++ api/index.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask import request
 import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -9,12 +10,16 @@ CORS(app)
 
 @app.route('/api/day')
 def day():
-    response = requests.get(
-        f'https://www.seoulouba.co.kr/campaign/?cat=&qq=&q=&q1=&q2=&ar1=&ar2=&ch[]=&sort=popular')
+    response = requests.get('https://www.seoulouba.co.kr/campaign/?cat=&qq=&q=&q1=&q2=&ar1=&ar2=&ch[]=&sort=popular')
+    html_text = response.text
 
-    html_text = response.text.replace('<!doctype html>', '').replace('<html','').replace('</html>','').replace('<','').replace('>','')
+    # BeautifulSoup을 사용하여 HTML 텍스트 파싱
+    soup = BeautifulSoup(html_text, 'html.parser')
 
-    return html_text
+    # 텍스트 형식으로 변환
+    text_content = soup.get_text()
+
+    return text_content
 
 
     
