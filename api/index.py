4921f705b22b4b10331f57e7c2ce08d576543a58
ryan3780 <guemhong3780@gmail.com>
2024-04-09 20:16:15 +0900

    Add api

--- api/index.py
+++ api/index.py
@@ -10,11 +10,4 @@ CORS(app)
 @app.route('/api/day')
 def day():
 
-    headers = {
-    }
-
-    response = requests.get(
-        f'https://api.weble.net/v1/campaigns?cat=%EC%A7%80%EC%97%AD&limit=35&media%5B%5D=blog&media%5B%5D=instagram&media%5B%5D=youtube&page=1&sort=latest&type=play', headers=headers)
-
-    print(response)
-    return response
+    return '??????'
