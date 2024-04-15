4bdc8fafd85ac6827c59774dc4a9056923131115
ryan3780 <guemhong3780@gmail.com>
2024-04-15 23:52:38 +0900

    Test

--- api/index.py
+++ api/index.py
@@ -35,7 +35,7 @@ def day():
         title = campaign.find('strong', 's_campaign_title')
         description = campaign.find('span', 'basic_blue')
         d_day = campaign.find('div', 'd_day')
-        minus_day = int(d_day.text.replace('D-',''))
+        minus_day = d_day.text.replace('D-','')
 
       
         info = {}
@@ -52,7 +52,7 @@ def day():
 
 
 
-        all_campaigns.append(info)
+        all_campaigns.append(minus_day)
 
     
 
