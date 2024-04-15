7d82383bd18d6db1a35ece1f2ca7e26f57d132f1
ryan3780 <guemhong3780@gmail.com>
2024-04-15 21:32:16 +0900

    Test

--- api/index.py
+++ api/index.py
@@ -17,18 +17,22 @@ def day():
     soup = BeautifulSoup(html_text, 'html.parser')
 
     # class='campaign_content'인 요소 찾기
-    tum_img_elements = soup.find_all('li','campaign_content')
+    campaign_contents = soup.find_all('li','campaign_content')
 
 
     all_campaigns = []
 
-    for tum_img in tum_img_elements:
-        a_tag = tum_img.find('a','tum_img')
+    for campaign in campaign_contents:
+        a_tag = campaign.find('a','tum_img')
         href = a_tag.get('href')
-        img = tum_img.find('img')
+        img = campaign.find('img')
         src = img.get('src')
 
-        all_campaigns.append(src)
+        href_and_src = {}
+        href_and_src['href'] = href
+        href_and_src['src'] = src
+
+        all_campaigns.append(href_and_src)
 
     
 
