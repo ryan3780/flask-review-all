908cbac683e6d5e362cb68d8592b786efaf9d033
ryan3780 <guemhong3780@gmail.com>
2024-04-18 13:06:10 +0900

    모집 인원

--- api/index.py
+++ api/index.py
@@ -28,30 +28,36 @@ def day():
     all_campaigns = []
 
     for campaign in campaign_contents:
-        a_tag = campaign.find('a','tum_img')
-        href = a_tag.get('href')
-        img = campaign.find('img')
-        src = img.get('src')
-        title = campaign.find('strong', 's_campaign_title')
-        description = campaign.find('span', 'basic_blue')
-        d_day = campaign.find('div', 'd_day')
 
+        d_day = campaign.find('div', 'd_day')
         minus_day = d_day.text.replace("\r", "").replace("\n", "").replace("\t", "").replace("D-", "")
 
-      
-        info = {}
-        info['href'] = href
-        info['src'] = src
-        info['title'] = title.text
-        info['description'] = description.text
-
-        if minus_day == 'day':
-            info['d_day'] = today.strftime('%Y-%m-%d')
+        if minus_day == '마감':
+            continue
         else:
-            int_minus_day = int(minus_day)
-            result_day = today - timedelta(days=int_minus_day)
-            info['d_day'] = result_day.strftime('%Y-%m-%d')
-            
+            a_tag = campaign.find('a','tum_img')
+            href = a_tag.get('href')
+            img = campaign.find('img')
+            src = img.get('src')
+            title = campaign.find('strong', 's_campaign_title')
+            description = campaign.find('span', 'basic_blue')
+            recruit = campaign.find('div', 'recruit')
+
+
+            info = {}
+            info['href'] = href
+            info['src'] = src
+            info['title'] = title.text
+            info['description'] = description.text
+            info['recruit'] = recruit.text
+
+            if minus_day == 'day':
+                info['d_day'] = today.strftime('%Y-%m-%d')
+            else:
+                int_minus_day = int(minus_day)
+                result_day = today - timedelta(days=int_minus_day)
+                info['d_day'] = result_day.strftime('%Y-%m-%d')
+        
 
 
 
